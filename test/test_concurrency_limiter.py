f6b854e5d7c69080a48859bca6145ca06293ba5d
Sylvain Mouquet <sylvain.mouquet@gmail.com>
2024-10-02 12:57:03 +0200

    feat(release): first release

--- test/test_concurrency_limiter.py
+++ test/test_concurrency_limiter.py
@@ -11,6 +11,8 @@ logging.getLogger("concurrency-limiter").setLevel(logging.DEBUG)
 
 @pytest.mark.asyncio
 async def test_concurrency_limiter():
+
+
     @concurrency_limiter(max_concurrent=5)
     async def fetch_data(id):
         logger.info(f"Fetch data: {id}")
